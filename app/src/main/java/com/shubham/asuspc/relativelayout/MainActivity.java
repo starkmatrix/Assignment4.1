0c097301f6ee4e04a25f0050fdf3f21c256b6d43
Shubham <sjalewa96@gmail.com>
2018-01-30 19:33:14 +0530

    Initial commit

---
+++ app/src/main/java/com/shubham/asuspc/relativelayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.shubham.asuspc.relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Click (View view)
+    {
+        Toast.makeText(MainActivity.this, "Welcome!!", Toast.LENGTH_SHORT).show();
+    }
+}
